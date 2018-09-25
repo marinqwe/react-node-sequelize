970b50fb063667fe5898b46fda0a005f85b37990
marinqwe <marinmarinbadrov@gmail.com>
2018-09-25 10:58:02 +0200

    Loader fix

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import '../styles/loader.css'
+import '../styles/loader.css';
 
 const Spinner = () => (
-    <div className="loader">Loading...</div>
+    <div className="loader">
+        <div>Loading...</div>
+        <div className="lds-circle" />
+    </div>
 );
 
 export default Spinner;
